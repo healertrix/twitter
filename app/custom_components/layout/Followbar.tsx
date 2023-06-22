9165a4a9be38b5a7e54031e30ddef306d33e9a79
abhinav <akum19cs@cmrit.ac.in>
2023-06-22 21:55:59 +0530

    Followbar error solved final

--- app/custom_components/layout/Followbar.tsx
+++ app/custom_components/layout/Followbar.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 export default function Followbar() {
   return (
     <div className='px-6 py-4 hidden lg:block'>
-          <div
-              className='bg-neutral-200 dark:bg-neutral-800 rounded-xl p-4'
-          >
-        Hello
+      <div className='bg-neutral-100 dark:bg-neutral-800 rounded-xl p-4'>
+              <h2 className='text-xl font-semibold'>Who to follow</h2>
+              <div className="flex flex-col gap-6 mt-4">
+                  {/* TODO */}
+              </div>
       </div>
     </div>
   );
