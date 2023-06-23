a50e5c05cddd0058700fcf5a31b85eb683280252
abhinav <akum19cs@cmrit.ac.in>
2023-06-23 11:47:25 +0530

    Login and Register modal done error resolve 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <RegisterModal />
-        <LoginModal />
+        {/* <RegisterModal /> */}
+        {/* <LoginModal /> */}
         <Layout>{children}</Layout>
       </body>
     </html>
